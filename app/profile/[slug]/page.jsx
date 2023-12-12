e7945fbc79f2ec76c0f3417748831ab1eb1d4d3f
Kursat Keskin <kursatkeskinn@gmail.com>
2023-12-12 12:03:19 +0000

    profile page update

--- app/profile/[slug]/page.jsx
+++ app/profile/[slug]/page.jsx
@@ -2,6 +2,7 @@
 import ItemCard from '@/components/ItemCard';
 import React, { useState, useEffect } from 'react';
 import { useSession, status } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 // Asynchronous function to get user data
 const getData = async (slug) => {
@@ -41,7 +42,8 @@ const getItem = async (slug) => {
 
 export default function Profile({ params }) {
   const { slug } = params;
-  const {session, status} = useSession()
+  const {data: session} = useSession()
+  const router = useRouter()
   const [user, setUser] = useState(null);
   const [items, setItems] = useState([]);
   const [error, setError] = useState(null);
@@ -68,31 +70,41 @@ export default function Profile({ params }) {
       });
   }, [slug]);
 
+  console.log(user)
+
   if (error) {
     return <div>Error loading data: {error}</div>;
   }
+
 
   return (
     <>
       <div className="profile-bg flex flex-col items-center">
         <div className="m-auto flex flex-col items-center">
-          <h1 className="background-h lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200">
-           {user ?  
-           <p>Welcome, {user.name}!</p> : 'Loading...'}
+          <h1 className="background-h">
+            {user ? (
+              <>
+              <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>Welcome to {user.name}'s Profile!</p>
+              <p className="background-p lg:text-xl text-lg lg:mt-4 mt-2 w-fit mx-auto px-4 text-gray-300">
+              Your journey has just started...
+            </p>
+            </>
+            ) : (
+              <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>Loading..</p>
+            )}
           </h1>
-          <p className="background-p lg:text-2xl text-lg lg:mt-4 mt-2 w-fit mx-auto px-4 text-gray-300">
-            Your journey has just started...
-          </p>
         </div>
       </div>
-      <div className='pt-10 min-h-screen max-w-[80%] mx-auto'>
-        <div className="text-2xl mt-5 font-bold ml-4 first-letter:uppercase">Your items on sale</div>
+      <div className="pt-10 min-h-screen max-w-[80%] mx-auto">
+        <div className="text-2xl mt-5 font-bold ml-4 first-letter:uppercase">
+          {user && `${user.name}'s items on sale`}
+        </div>
         <hr className="my-2" />
         <div className="flex mt-10 justify-center items-center flex-wrap mx-auto gap-10">
-    {items.map(item => (
-          <ItemCard key={item.id} item={item} />
-        ))}
-    </div>
+          {items.map((item) => (
+            <ItemCard key={item.id} item={item} />
+          ))}
+        </div>
       </div>
     </>
   );
