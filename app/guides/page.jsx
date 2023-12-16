c6e4096a728ea6a12b37f0425cb539d00001cdac
Kursat Keskin <kursatkeskinn@gmail.com>
2023-12-16 17:42:49 +0000

    bug fix

--- app/guides/page.jsx
+++ app/guides/page.jsx
@@ -1,9 +1,9 @@
 import CardList from '@/components/CardList'
 import Menu from '@/components/Menu'
-import React, { Suspense } from 'react'
+import React from 'react'
  
 export const metadata = {
-  title: 'Albion Online Guides | Master the Game with Expert Strategies |  Albion Journey',
+  title: 'Albion Online Guides | Master the Game with Expert Strategies | Albion Journey',
   description: 'Dive into our extensive collection of Albion Online guides. From beginner tips to advanced strategies, Albion Journey offers everything you need to enhance your gameplay and dominate the world of Albion. Start your strategic journey today!',
 }
 export default function Guides({searchParams}) {
