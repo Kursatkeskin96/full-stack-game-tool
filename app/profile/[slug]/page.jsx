bebe35e9cfca33d8690f19cab31fb50fc4f65a2e
Kursat Keskin <kursatkeskinn@gmail.com>
2023-12-21 23:09:26 +0000

    referal reward removed

--- app/profile/[slug]/page.jsx
+++ app/profile/[slug]/page.jsx
@@ -145,116 +145,35 @@ export default function Profile({ params, searchParams }) {
   }
   return (
     <>
-    <title>Manage Your Profile & Listings in Albion Online | Albion Journey</title>
+      <title>Manage Your Profile & Listings in Albion Online | Albion Journey</title>
       <div className="profile-bg flex flex-col items-center">
         <div className="m-auto flex flex-col items-center">
           <h1 className="background-h">
             {user ? (
               <>
-              <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>Welcome to {user.name}&apos;s Profile!</p>
-              <p className="background-p lg:text-xl text-lg lg:mt-4 mt-2 w-fit mx-auto px-4 text-gray-300">
-              Your journey has just started...
-            </p>
-            </>
+                <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>
+                  Welcome to {user.name}&apos;s Profile!
+                </p>
+                <p className="background-p lg:text-xl text-lg lg:mt-4 mt-2 w-fit mx-auto px-4 text-gray-300">
+                  Your journey has just started...
+                </p>
+              </>
             ) : (
-              <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>Loading..</p>
+              <p className='lg:text-5xl text-xl w-fit mx-auto px-4 text-gray-200'>Loading...</p>
             )}
           </h1>
         </div>
       </div>
-      <div className="pt-10 min-h-screen max-w-[80%] mx-auto">
-        {user && session ? (
-          <>
-        {
-  session.user.name == slug ? (
-    <div>
-      {
-        user.referredby == null ? (
-          <div>
-  <div className="text-2xl mt-10 font-bold ml-4 first-letter:uppercase">
-          Referred By
-        </div>
-        
-        <hr className="my-2" />
-        <div className='flex flex-col justify-center items-center gap-4'>
-        <div className='text-lg ml-4 text-center'>If you have referred by someone else, you can write that user&apos;s discord nick in search. </div>
-        <div className='text-md ml-4 text-center'>We have valuable rewards for who refer our website.  </div>
-        <div className='text-sm ml-4 text-center'>          <ul>
-          <li className='flex'><span className='font-bold w-16'>10 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 30x Tome Of Insight </li>
-          <li className='flex '><span className='font-bold w-16'>50 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 200x Tome Of Insight</li>
-          <li className='flex'><span className='font-bold w-16'>100 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 500x Tome Of Insight</li>
-          </ul></div>
-          </div>
-        <div className='flex mt-10 justify-center items-center gap-2'>
-<div className="input-icon-container">
-    <input 
-    placeholder='search user'
-    value={searchTerm}
-    onChange={handleSearchChange}
-        type="text" 
-        className="w-60 h-8 rounded-md pl-8 border-2 border-black text-black" // Note the left padding
-    />
-    <IoSearchOutline className="input-icon"/>
-</div>
-      <button 
-           onClick={handleSearchSubmit}
-        className="bg-blue-500 text-white w-20 h-8 rounded-md"
-      >
-        Search
-      </button>
-    </div>
-    {refer ? (
-        <div className='flex justify-center items-center gap-4'>
-        <div className='flex mt-5 w-fit mx-auto px-2 py-1 rounded-md text-xl justify-center items-center gap-2'><span><Image  className='rounded-[50%]' src={refer.image} width={30} height={30} alt='dcpic' /></span> {refer.name} <button className='bg-green-600 px-1 text-sm py-1 w-14 rounded-md text-white' onClick={handleRef}>Select</button></div>
-        </div>
-      ): (
-<div></div>
-
-      )}
-          </div>
-        ) : (
-          <div>    
-               <div className="text-2xl mt-10 font-bold  ml-4 first-letter:uppercase">
-          Refer A Friend And Earn Tones Of Silver!
-        </div>
-        <hr className="my-2" />
-        <div className='flex my-10 justify-center gap-10 items-center flex-col lg:flex-row md:flex-row                        '>
-      <div className=''>
-      <p className='text-lg'>{user.name}&apos;s refer count: <span className=' text-orange-600'>{refcount}</span></p>
-        <p className='text-md'>{user.name} has refered by <span className='underline italic'>{user.referredby}</span></p>
-      </div>
-      <div className='text-sm'>
-          <ul>
-          <li className='flex'><span className='font-bold w-16'>10 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 30x Tome Of Insight </li>
-          <li className='flex '><span className='font-bold w-16'>50 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 200x Tome Of Insight</li>
-          <li className='flex'><span className='font-bold w-16'>100 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 500x Tome Of Insight</li>
-          </ul>
-          </div>
-      </div>
-       </div>
-        )
-      }
-    </div>
-  ) : (
-    <div></div>
-  )
-}
-
-</>
-): <p>
-  
-</p> }
-<div className="text-2xl mt-5 font-bold ml-4 first-letter:uppercase">
-          {user && `${user.name}'s items on sale`}
-        </div>
-        <hr className="my-2" />
-        <div className="flex mt-10 justify-center items-center flex-wrap mx-auto gap-10">
-          {items.map((item) => (
-            <ItemCard key={item.id} item={item} />
-          ))}
-        </div>
-      </div>
       
+      <div className="text-2xl mt-10 font-bold ml-4 first-letter:uppercase">
+        {user && `${user.name}'s items on sale`}
+      </div>
+      <hr className="my-2" />
+      <div className="flex mt-10 justify-center items-center flex-wrap mx-auto gap-10">
+        {items.map((item) => (
+          <ItemCard key={item.id} item={item} />
+        ))}
+      </div>
     </>
   );
 }
--- components/Info.jsx
+++ components/Info.jsx
@@ -31,23 +31,6 @@ function Info() {
         </div>
     </div>
 
-<div className='flex justify-center items-center'>
-<h4 className=" text-4xl mt-20 font-bold gradient text-center mx-auto flex lg:flex-row md:flex-row flex-col gap-4">
-<LuGift className='text-[#00DBDE] text-center mx-auto' /> Refer Your Friends & Earn Rewards!<LuGift className='text-[#16A34A] text-center mx-auto' />
-</h4>
-</div>
-
-<div className='max-w-[80%] flex-col gap-2 mt-6 mx-auto flex justify-center text-center items-center'>
-<p className='text-lg'>Welcome to Albion Journey, where your connections bring you rewards! We are excited to introduce our Refer & Reward System, a program designed to thank you for spreading the word about our community. It is simple, rewarding, and adds an extra layer of fun to your experience with us.</p>
-<p>All you need to do is refer your friends and encourage them to enter your Discord username in the input field on their <span className='font-bold underline'>profile</span> page. That is all it takes! You can also easily track how many referrals you have. It is that simple! Start sharing and watch your rewards grow as your referrals increase.</p>
-<div className='text-md ml-4 mt-5 text-center'>          <ul>
-          <li className='flex'><span className='font-bold w-20'>10 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 30x Tome Of Insight </li>
-          <li className='flex '><span className='font-bold w-20'>50 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 200x Tome Of Insight</li>
-          <li className='flex'><span className='font-bold w-20'>100 refer:</span><span className='ml-2'><Image src='https://render.albiononline.com/v1/item/T4_SKILLBOOK_STANDARD' width={20} height={20} alt='tome-of-insight' /></span> 500x Tome Of Insight</li>
-          </ul></div>
-</div>          
-<p className='text-center text-sm italic mx-auto mt-10'>This refer & reward system will last until January 20, 2024.</p>
-
     <hr className='mt-20 w-[80%] mx-auto'></hr>
 
     <div className='flex mt-10 justify-center items-center lg:gap-20 lg:max-w-[80%] max-w-[90%] mx-auto flex-wrap'>
