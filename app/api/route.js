b7e2855d808edd4e6bc87e1e8931e50511842277
Kursat Keskin <kursatkeskinn@gmail.com>
2024-01-14 11:38:52 +0000

    cors changed

--- app/api/route.js
+++ app/api/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   return new Response("Hello, Next.js!", {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://www.albionjourney.com/",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
