f1ee52b154d64373db66f36b9f4b70f605523bb5
Kursat Keskin <kursatkeskinn@gmail.com>
2024-01-14 00:25:43 +0000

    guides metadata removed

--- app/guides/page.jsx
+++ app/guides/page.jsx
@@ -2,10 +2,7 @@ import CardList from '@/components/CardList'
 import Menu from '@/components/Menu'
 import React from 'react'
  
-export const metadata = {
-  title: 'Albion Online Guides | Master the Game with Expert Strategies | Albion Journey',
-  description: 'Dive into our extensive collection of Albion Online guides. From beginner tips to advanced strategies, Albion Journey offers everything you need to enhance your gameplay and dominate the world of Albion. Start your strategic journey today!',
-}
+
 export default function Guides({searchParams}) {
 const page = parseInt(searchParams.page) || 1
 
